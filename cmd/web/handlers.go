adc053422cb68c1fee7283d964691508bb6f0693
Marshall Humble <11792578+marshallhumble@users.noreply.github.com>
2024-07-21 20:45:44 -0500

    Added SQL setup script

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -481,22 +481,6 @@ func (app *application) editUserPost(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	/*
-		form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
-		form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
-		form.CheckField(validator.Matches(form.Email, validator.EmailRX),
-			"email", "This field must be a valid email address")
-		form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
-		form.CheckField(validator.MinChars(form.Password, 8), "password",
-			"This field must be at least 8 characters long")
-
-		if !form.Valid() {
-			data := app.newTemplateData(r)
-			data.Form = form
-			app.render(w, r, http.StatusUnprocessableEntity, "user_edit.gohtml", data)
-			return
-		}
-	*/
 
 	user, err := app.users.UpdateUser(id, form.Name, form.Email, form.Password, form.Admin)
 
